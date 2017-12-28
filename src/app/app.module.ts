53f3fd2675ca6fd49ef46fea81b38dbf746a704d
valhadreams <valhadreams@gmail.com>
2017-12-28 21:45:22 +0900

    Health diary UI - Calendar UI - Input event UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {APP_BASE_HREF, HashLocationStrategy, LocationStrategy} from "@angular/common";
+import { HashLocationStrategy, LocationStrategy} from "@angular/common";
 import {appRouter} from './app.router';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {DiaryComponent} from "./components/diary/diary.component";
 import {MaterialModule} from "./material.module";
-import {NavComponent} from "./components/diary/nav/nav.component";
-import {SidebarComponent} from "./components/diary/sidenav/sidenav.component";
-import {ContentComponent} from "./components/diary/content/content.component";
+import {SideNavComponent} from "./components/home/side-nav/side-nav.component";
+import {BodyInfoComponent} from "./components/home/content/body-info/body-info.component";
+import {HealthDiaryComponent} from "./components/home/content/health-diary/health-diary.component";
+import {BodyAnalysisComponent} from "./components/home/content/body-analysis/body-analysis.component";
+import {HomeComponent} from "./components/home/home.component";
+
+const components = [
+  AppComponent,
+  HomeComponent,
+  SideNavComponent,
+  BodyInfoComponent,
+  HealthDiaryComponent,
+  BodyAnalysisComponent
+];
 
 @NgModule({
-  declarations: [AppComponent, DiaryComponent, NavComponent, SidebarComponent, ContentComponent],
+  declarations: components,
   imports: [BrowserAnimationsModule, MaterialModule, appRouter],
   providers: [{ provide : LocationStrategy, useClass : HashLocationStrategy }],
   bootstrap: [AppComponent]
--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,12 +1,23 @@
 import {RouterModule, Routes} from "@angular/router";
 import {ModuleWithProviders} from "@angular/core";
-import {DiaryComponent} from "./components/diary/diary.component";
+import {BodyInfoComponent} from "./components/home/content/body-info/body-info.component";
+import {HealthDiaryComponent} from "./components/home/content/health-diary/health-diary.component";
+import {BodyAnalysisComponent} from "./components/home/content/body-analysis/body-analysis.component";
+import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
-  { path : '', redirectTo : 'diary', pathMatch : 'full' },
-  { path : 'diary', component : DiaryComponent },
+  { path : '', redirectTo : 'home', pathMatch : 'full' },
+  {
+    path : 'home', component : HomeComponent,
+    children : [
+      { path : '', component : HealthDiaryComponent },
+      { path : 'body-info', component : BodyInfoComponent },
+      { path : 'body-analysis', component : BodyAnalysisComponent }
+    ]
+  },
   { path : 'sign-in', loadChildren : 'app/components/sign/signin/sign.in.module#SignInModule' },
   { path : 'sign-up', loadChildren : 'app/components/sign/signup/sign.up.module#SignUpModule' }
 ];
 
 export const appRouter : ModuleWithProviders = RouterModule.forRoot(routes);
+
